8cd9dd33cc7ab7a393bee3f9b70f2dde3ea3527a
alvinjin <alvincjin@gmail.com>
2017-08-11 10:08:19 -0400

    Fixed send_email out of app context issue

--- app/emails.py
+++ app/emails.py
@@ -1,35 +1,38 @@
-from flask import render_template
+from flask import render_template, current_app
 from flask_mail import Message
 from app import mail
 from decorators import async
 from config import ADMINS
 
+
 @async    
-def send_async_email(msg):
-    mail.send(msg)
-    
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender = sender, recipients = recipients)
+    app = current_app._get_current_object()
+    msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    send_async_email(msg)
+    send_async_email(app, msg)
    
 
 def send_emails(subject, text_body, recipient):
-    recipients_list = []
+    app = current_app._get_current_object()
+    recipients_list = list()
     recipients_list.append(recipient)
-    msg = Message(subject, sender = ADMINS[0], recipients = recipients_list)
+    msg = Message(subject, sender=ADMINS[0], recipients=recipients_list)
     msg.body = text_body
-    send_async_email(msg)
+    send_async_email(app, msg)
     
 
-    
 def follower_notification(followed, follower):
-    send_email("[microblog] %s is now following you!" % follower.nickname,
+    send_email(
+        "[microblog] %s is now following you!" % follower.nickname,
         ADMINS[0],
         [followed.email],
-        render_template("follower_email.txt", 
-            user = followed, follower = follower),
-        render_template("follower_email.html", 
-            user = followed, follower = follower))
-        
+        render_template("follower_email.txt", user=followed, follower=follower),
+        render_template("follower_email.html", user=followed, follower=follower)
+    )
