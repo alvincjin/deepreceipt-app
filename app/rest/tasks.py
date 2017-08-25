5f99b31445191cd5b29b3f8557a197db1fb7eeb1
alvinjin <alvincjin@gmail.com>
2017-08-25 16:36:58 -0400

    added flask-restful

--- app/rest/tasks.py
+++ app/rest/tasks.py
@@ -2,9 +2,10 @@ from flask import Flask, jsonify, abort, make_response
 from flask_restful import Resource, reqparse, fields, marshal
 from flask_httpauth import HTTPBasicAuth
 from app import restApi
+from ..models import User, Post
+
 
 auth = HTTPBasicAuth()
-
 
 @auth.get_password
 def get_password(username):
@@ -39,6 +40,32 @@ task_fields = {
     'description': fields.String,
     'done': fields.Boolean,
     'uri': fields.Url('task')
+}
+
+
+user_fields = {
+    'id': fields.Integer,
+    'nickname': fields.String,
+    'firstname': fields.String,
+    'lastname': fields.String,
+    'email': fields.String,
+    'phone': fields.Integer,
+    'address': fields.String
+}
+
+
+post_fields = {
+    'id': fields.Integer,
+    'title': fields.String,
+    'body': fields.String,
+    'user_id': fields.Integer,
+    'location': fields.String,
+    'price': fields.Integer,
+    'address': fields.String,
+    'badroom_no': fields.Integer,
+    'garage_no': fields.Integer,
+    'bathroom_no': fields.Integer,
+    'style': fields.String
 }
 
 
@@ -103,5 +130,57 @@ class TaskAPI(Resource):
         tasks.remove(task[0])
         return {'result': True}
 
+
+class UserAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('id', type=int, required=True,
+                                   help='No task title provided',
+                                   location='json')
+        self.reqparse.add_argument('nickname', type=str, default="", location='json')
+        self.reqparse.add_argument('firstname', type=str, default="", location='json')
+        self.reqparse.add_argument('lastname', type=str, default="", location='json')
+        self.reqparse.add_argument('email', type=str, default="", location='json')
+        self.reqparse.add_argument('phone', type=str, default="", location='json')
+        self.reqparse.add_argument('address', type=str, default="", location='json')
+
+        super(UserAPI, self).__init__()
+
+    def get(self, id):
+        user = User.query.get_or_404(id)
+
+        return {'user': marshal(user, user_fields)}
+
+
+class PostAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('id', type=int, required=True,
+                                   help='No task title provided',
+                                   location='json')
+        self.reqparse.add_argument('title', type=str, default="", location='json')
+        self.reqparse.add_argument('body', type=str, default="", location='json')
+        self.reqparse.add_argument('user_id', type=int, default="", location='json')
+        self.reqparse.add_argument('location', type=str, default="", location='json')
+        self.reqparse.add_argument('price', type=int, default="", location='json')
+        self.reqparse.add_argument('address', type=str, default="", location='json')
+        self.reqparse.add_argument('badroom_no', type=int, default="", location='json')
+        self.reqparse.add_argument('garage_no', type=int, default="", location='json')
+        self.reqparse.add_argument('bathroom_no', type=int, default="", location='json')
+        self.reqparse.add_argument('style', type=str, default="", location='json')
+
+        super(PostAPI, self).__init__()
+
+    def get(self, id):
+        post = Post.query.get_or_404(id)
+
+        return {'post': marshal(post, post_fields)}
+
 restApi.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
 restApi.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
+restApi.add_resource(UserAPI, '/users/<int:id>', endpoint='user')
+restApi.add_resource(PostAPI, '/posts/<int:id>', endpoint='post')
